8569a9f6c373052e5194b0eb168ecc45051de274
datotoda <datotoda1342@gmail.com>
2022-02-05 22:31:44 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Calendar Generator
+
+This is a Python written calendar generator program that generates calendars by month in HTML format. You can also write the desired number in weeks. I have used Jinja 2 and Calendar built-in module. 
+
+
+---
+
+# Documentations
+
+- [Calendar Documentation](https://docs.python.org/3/library/calendar.html)
+  
+- [Jinja Documentation](https://jinja.palletsprojects.com/en/3.0.x/)
+
+# Install requirements
+
+``$ pip install -r requirements.txt``
---
+++ main.py
@@ -0,0 +1,38 @@
+from jinja2 import Template
+import calendar
+
+month_names = ['იანვარი', 'თებერვალი', 'მარტი', 'აპრილი', 'მაისი', 'ივნისი', 'ივლისი', 'აგვისტო', 'სექტემბერი', 'ოქტომბერი', 'ნოემბერი', 'დეკემბერი']
+year = 2022  # start year
+month = 1  # start month
+n, max_n = -7, 20  # range for n
+
+def get_year_and_month(year, month):
+    months_sum = year * 12 + month - 1
+    return months_sum // 12, months_sum % 12 + 1
+
+
+html_cal = calendar.HTMLCalendar(firstweekday = 0)
+
+with open('./template.html', 'r', encoding='utf-8') as f:
+    t = Template(f.read())
+
+for month_count in range(9):
+    y, m = get_year_and_month(year, month + month_count)
+    rows = html_cal.formatmonth(y, m).split('\n')[3:-2]
+
+    content = {
+        'month': f'{month_names[m-1]} {y}',
+        'row_height': 660//len(rows),
+        'table': ''}
+    for line in rows:
+        content['table'] += f'\t\t\t{line[:4]} <th>{n if 0 < n <= max_n else ""}</th> {line[4:]}\n'
+        n += 1
+    else:
+        if 'noday' in line:
+            n -= 1
+        content['table'] = content['table'][:-1]
+    
+    # print(t.render(**content))
+
+    with open(f'./2022/{m}_{y}.html', 'w', encoding='utf-8') as f:
+        f.write(t.render(**content))
